84ec71ba01a104bbdaa7558856fead7d9113616f
MaddLiebert <email_github_lu@gmail.com>
2026-08-26 22:43:47 +0700

    feat: setup PWA, responsive design, audio optimizations, search filter

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['**/*'],
+      manifest: {
+        name: 'Japanese Quiz',
+        short_name: 'JLPT N5',
+        description: 'Offline capable Japanese Quiz app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,woff,woff2,mp3,wav}']
+      }
+    })
+  ],
 })
